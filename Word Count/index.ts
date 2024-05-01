512c41213e467c95e8f28a365f70b6ec87178c29
Noureen-Akhtar <164550138+Noureen-Akhtar@users.noreply.github.com>
2024-05-01 13:56:40 +0500

    initial commit

---
+++ Word Count/index.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+console.log(chalk.yellow.bold(`
+
+                       ____________________________________________
+                      |                                            |
+                      |         Word And Character Counter         |
+                      |                                            |
+                      |____________________________________________|                                            |
+
+`));
+
+//prompt user to enter text/sentence
+const wordCharCount = await inquirer.prompt([
+    {
+        name: 'text',
+        type: 'input',
+        message: 'Enter the Text to count the words and characters: ', 
+    }   
+]);
+
+const text = wordCharCount.text
+
+//trim() removes whitespace from both ends of a string and split string into an array without spaces
+const words = text.trim().split(" ").length;
+
+//line spacing
+console.log("\n");
+
+//console word count
+console.log(chalk.blue.bold(`Words: ${words}`));
+
+//split string into an array without spaces and join (concatenate) elements of an array  into a single string
+const charCount = text.split(" ").join("").length;
+
+//console chararacter count
+console.log(chalk.blue.bold(`Characters: ${charCount}`));
+
+//consle ending lines
+console.log (chalk.cyan.bold("Thank you for using Word Counter\n"));
